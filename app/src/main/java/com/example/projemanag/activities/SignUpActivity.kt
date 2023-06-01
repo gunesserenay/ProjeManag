6fa984bccf8a314f980daf348ed6f09b5fc602bf
Serenay <serenayygunes@gmail.com>
2023-06-01 20:49:45 +0300

    Preparing The Signup Feature in Firebase

--- app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -3,12 +3,14 @@ package com.example.projemanag.activities
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     private var binding:ActivitySignUpBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +41,36 @@ class SignUpActivity : AppCompatActivity() {
 
         binding?.toolbarSignUpActivity?.setNavigationOnClickListener {
             onBackPressed()
+        }
+        binding?.btnSignUp?.setOnClickListener {
+            registerUser()
+        }
+    }
+    private fun registerUser(){
+        val name:String=binding?.etName?.text.toString().trim { it <= ' ' }
+        val email:String=binding?.etEmail?.text.toString().trim { it <= ' ' }
+        val password:String=binding?.etPassword?.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name,email,password)){
+            Toast.makeText(this,"Now we can register a new user",Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun validateForm(name:String,email:String, password:String):Boolean{
+        return when{
+            TextUtils.isEmpty(name)-> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email)-> {
+                showErrorSnackBar("Please enter an email")
+                false
+            }
+            TextUtils.isEmpty(password)-> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }else->{
+                true
+            }
         }
     }
     override fun onDestroy() {
