31b0c776135a6188e4c7238c7c4c263266d9d462
Serenay <serenayygunes@gmail.com>
2023-06-05 12:32:17 +0300

    Sign up via email in Firebase

--- app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -9,6 +9,8 @@ import android.view.WindowManager
 import android.widget.Toast
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignUpActivity : BaseActivity() {
     private var binding:ActivitySignUpBinding?=null
@@ -52,7 +54,22 @@ class SignUpActivity : BaseActivity() {
         val password:String=binding?.etPassword?.text.toString().trim { it <= ' ' }
 
         if (validateForm(name,email,password)){
-            Toast.makeText(this,"Now we can register a new user",Toast.LENGTH_SHORT).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                task->
+                hideProgressBar()
+                if (task.isSuccessful){
+                    val firebaseUser:FirebaseUser=task.result!!.user!!
+                    val registeredEmail=firebaseUser.email!!
+                    Toast.makeText(this , "$firebaseUser $registeredEmail successfully",Toast.LENGTH_LONG).show()
+                    FirebaseAuth.getInstance().signOut()
+                    finish()
+                }else{
+                    Toast.makeText(this , task.exception!!.message,Toast.LENGTH_LONG).show()
+
+                }
+            }
         }
     }
     private fun validateForm(name:String,email:String, password:String):Boolean{
